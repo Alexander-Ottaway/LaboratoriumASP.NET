5c9390bb61d669bcaecc5b3b2cbc04a436603a58
Alexander-Ottaway <alexander.ottaway@microsoft.wsei.edu.pl>
2024-10-08 18:23:03 +0200

    zrobione na zajęciach

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
     public IActionResult Index()
     {
         return View();
@@ -22,10 +26,65 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Kalkulator(Operator? op, double? x, double? y)
+    {
+       // var op = Request.Query["op"];
+        //var x = double.Parse(Request.Query["x"]);
+        //var y = double.Parse(Request.Query["y"]);
+
+        if (x is not null && y is null && op == Operator.Sin)
+        {
+            ViewBag.Result = Math.Sin((double)x);
+        }
+        else if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format parametru x lub y";
+            return View("CalculatorError");
+        }
+        else
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format zapytania";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorError"); // w przypadku złego operatora
+        }
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.Result = x + y;
+                break;
+            case Operator.Sub:
+                ViewBag.Result = x - y;
+                break;
+            case Operator.Mul:
+                ViewBag.Result = x * y;
+                break;
+            case Operator.Div:
+                ViewBag.Result = x / y;
+                break;            
+            case Operator.Pow:
+                ViewBag.Result = Math.Pow((double)x, (double)y);
+                break;
+        }
+        
+        return View();
+    }
+
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public enum Operator
+{
+    Add, Sub, Mul, Div, Pow, Sin
 }
